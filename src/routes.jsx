a7c0c0c3cf44a9bc75fcf759d92d27c6226520f6
Elton Santos <eltonaxl@hotmail.com>
2023-02-21 03:06:56 -0300

    Finish app

---
+++ src/routes.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Favoritos from "./pages/Favoritos";
+import Filme from "./pages/Filme";
+import Home from "./pages/Home";
+
+import Erro from "./pages/Erro";
+
+import Header from "./components/Header";
+
+function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+
+        <Route path="*" element={<Erro />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RoutesApp;
